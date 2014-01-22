d7eefb5cd2ecc72faa70629976405136afe60b44
Ludovic Valente <ludovic.valente@gmail.com>
2014-01-22 15:15:46 +0100

    v1.2 with password protection

--- bin/run.bat
+++ bin/run.bat
@@ -1,1 +1,11 @@
-java -jar monitor.jar
+rem java -jar monitor.jar
+
+rem *************************************************
+rem to run password protection
+rem *************************************************
+java -jar monitor.jar -ciphered
+
+rem *************************************************
+rem to run password protection with password as input (script) 
+rem *************************************************
+rem java -jar monitor.jar -ciphered -secret xyz
